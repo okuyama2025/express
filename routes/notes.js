40e091789084ec1be7660b8a9707fd8529f1e07f
okuyama2025 <2201100024rh@cyber-u.ac.jp>
2025-06-04 18:09:44 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,31 @@
 var express = require('express');
+var router = express.Router();
+
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://okuyama2025:okuyama000@cluster0.6hikbes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
+res.json(note);
+})
+
+module.exports = router;
+
+
+
+
+//old
+/*
+var express = require('express');
  var router = express.Router();
  // レスポンスのデータ（ノート全件）
 const responseObjectDataAll = {
@@ -22,8 +49,10 @@ const responseObjectDataAll = {
  * @returns {string} data.title - タイトル
 * @returns {string} data.text - 内容
 */
+/*
  router.get('/', function (req, res, next) {
  // 全件取得して返す
 res.json(responseObjectDataAll);
  })
  module.exports = router;
+*/
