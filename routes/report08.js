9ca4faa99dfd57a64856b5ab96d901b47de77f24
okuyama2025 <2201100024rh@cyber-u.ac.jp>
2025-06-04 21:26:09 +0900

    add report08

--- routes/report08.js
+++ routes/report08.js
@@ -3,7 +3,7 @@ var router= express.Router();
 const request= require('request');
 
 router.get('/', async(req, res) =>{
-    request('https://hellosalut.stefanbohacek.dev/?lang=ja', function(error, response,body) {
+    request('https://official-joke-api.appspot.com/jokes/random', function(error, response,body) {
         if(!error&& response.statusCode== 200) {
             const data= JSON.parse(body);
             res.json(data);
