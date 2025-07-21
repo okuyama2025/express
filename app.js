a0a75864c946474b3f48fa15e80730988bc6631c
okuyama2025 <2201100024rh@cyber-u.ac.jp>
2025-07-21 18:49:33 +0900

    add env

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 var report08Router = require('./routes/report08');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -28,7 +29,8 @@ app.use('/users', usersRouter);
 app.use('/hello',helloRouter);
 app.use('/notes',notesRouter);
 app.use('/cat', catRouter);
-app.use('/report08', report08Router)
+app.use('/report08', report08Router);
+app.use('/notes_from_b',notes_from_bRouter);
 
 
 // catch 404 and forward to error handler
--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const cors=require('cors');//CORSミドルウェアを追加
+const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
 // 接続情報を設定
@@ -8,14 +8,17 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
 
 res.json(note);
 })
